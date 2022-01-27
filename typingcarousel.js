d66d3236ce6af26d690561ec08bdf5ea5b427c1c
Macm91 <macm91.mc@gmail.com>
2022-01-26 21:45:20 -0500

    moved intro section to above navigation.toying with styling

--- typingcarousel.js
+++ typingcarousel.js
@@ -51,6 +51,6 @@ var TxtRotate = function(el, toRotate, period) {
     // INJECT CSS
     var css = document.createElement("styleSheet");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    // css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
   };
